a412963efcf8009ec21aa251d174aa8a570d4610
JupiterRoue45 <idrisbenarbia1@gmail.com>
2025-02-13 18:51:43 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace cleaner
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
